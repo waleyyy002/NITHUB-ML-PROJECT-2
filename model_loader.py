dffdeee6bf4a86ab5611415768584f17e6a9cb8c
Your Name <you@example.com>
2026-08-05 21:48:33 +0100

    Fix HF auth and low-memory model loading

--- model_loader.py
+++ model_loader.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment-latest"
@@ -8,13 +9,25 @@ HF_TOKEN = os.environ.get("HF_TOKEN")
 use_local = os.path.isdir(LOCAL_MODEL_DIR)
 model_source = LOCAL_MODEL_DIR if use_local else MODEL_NAME
 
-load_kwargs = {
+# Limit CPU threads to reduce memory usage on constrained hosts
+torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
+
+tokenizer_kwargs = {
+    "local_files_only": use_local,
+}
+
+model_kwargs = {
     "local_files_only": use_local,
     "low_cpu_mem_usage": True,
 }
 
 if not use_local and HF_TOKEN:
-    load_kwargs["use_auth_token"] = HF_TOKEN
+    tokenizer_kwargs["use_auth_token"] = HF_TOKEN
+    model_kwargs["use_auth_token"] = HF_TOKEN
 
-tokenizer = AutoTokenizer.from_pretrained(model_source, **load_kwargs)
-model = AutoModelForSequenceClassification.from_pretrained(model_source, **load_kwargs)
+# CPU-backed float16 may not be fully supported on all hosts, so we use default dtype
+# and low_cpu_mem_usage to reduce the model footprint.
+
+tokenizer = AutoTokenizer.from_pretrained(model_source, **tokenizer_kwargs)
+model = AutoModelForSequenceClassification.from_pretrained(model_source, **model_kwargs)
