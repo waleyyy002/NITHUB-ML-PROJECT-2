2ddb464a7e3cd3d40dc3b45ab46e4584d81c20fa
Your Name <you@example.com>
2026-08-05 21:25:21 +0100

    update

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,20 @@
+import os
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+LOCAL_MODEL_DIR = "twitter-roberta-base-sentiment-latest"
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+use_local = os.path.isdir(LOCAL_MODEL_DIR)
+model_source = LOCAL_MODEL_DIR if use_local else MODEL_NAME
 
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+load_kwargs = {
+    "local_files_only": use_local,
+    "low_cpu_mem_usage": True,
+}
+
+if not use_local and HF_TOKEN:
+    load_kwargs["use_auth_token"] = HF_TOKEN
+
+tokenizer = AutoTokenizer.from_pretrained(model_source, **load_kwargs)
+model = AutoModelForSequenceClassification.from_pretrained(model_source, **load_kwargs)
